4c40fd2411f766bee15b95a7b62f0baf04a0fb1a
OshaX <87685400+oshaxx@users.noreply.github.com>
2025-02-26 21:17:03 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight',
+  packname: 'Osha Xz',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botName: "Osha Xz Bot",
+  botOwner: 'Osha Xz', // Your name
+  ownerNumber: '94782722212', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
